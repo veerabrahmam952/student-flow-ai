50e6faa864486ed2547d22c8d98104ffa880077f
Veera Brahmam <77602611+veerabrahmam952@users.noreply.github.com>
2025-09-10 10:08:00 +0000

    Modify Dev.inx files

--- idx-template.nix
+++ idx-template.nix
@@ -1,22 +1,17 @@
-{ pkgs, language ? "js", ... }: {
-  packages = [
-    pkgs.nodejs_20
-  ];
+# No user-configurable parameters
+{ pkgs, ... }: {
+  # Shell script that produces the final environment
   bootstrap = ''
-    mkdir -p "$WS_NAME"
-    npm create -y vite@latest "$WS_NAME" -- --template ${if language == "ts" then "react-ts" else "react"}
-    mkdir -p "$WS_NAME/.idx/"
-    cp -rf ${./icon.png} "$WS_NAME/.idx/icon.png"
-    cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
-    chmod -R +w "$WS_NAME"
-    mv "$WS_NAME" "$out"
+    # Copy the folder containing the `idx-template` files to the final
+    # project folder for the new workspace. ${./.} inserts the directory
+    # of the checked-out Git folder containing this template.
+    cp -rf ${./.} "$out"
 
-    mkdir -p "$out/.idx"
-    chmod -R u+w "$out"
-    cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
-    cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
-    chmod -R u+w "$out"
+    # Set some permissions
+    chmod -R +w "$out"
 
-    cd "$out"; npm install --package-lock-only --ignore-scripts
+    # Remove the template files themselves and any connection to the template's
+    # Git repository
+    rm -rf "$out/.git" "$out/idx-template".{nix,json}
   '';
 }
